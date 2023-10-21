aec7f4a3e46ae86274290c6ffb6dfc2416304cec
lvega77 <112046842+lvega77@users.noreply.github.com>
2023-10-21 10:29:56 -0500

    Reservation at models.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import User
+from .models import Reservation
 
 #register models
 class UserAdmin(admin.ModelAdmin):
     list_display = ('UserFirstName', 'UserLastName', 'Email', 'State')
     list_filter = ('State',)
 
-admin.site.register(User)
 
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('ReservationID', 'TimeOfReservation', 'Date', 'UserID')
+    list_filter = ('Date', 'TimeOfReservation')
+
+admin.site.register(User)
+admin.site.register(Reservation, ReservationAdmin)
